0e2ff9da745373688b7a82723467f2d55a7f9691
XaV1Uzz <xaviuzz@gmail.com>
2014-07-09 11:52:05 +0200

    A Triangle  should have all sides as numbers

--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -2,11 +2,11 @@ require './triangle'
 
 describe "A Triangle " do
   it "is also a Polygon" do
-    expect(Triangle.new(1,2,3).class.ancestors).to include(Polygon)
+    expect(Triangle.new(2,2,3).class.ancestors).to include(Polygon)
   end
 
   it "has three sides" do
-    expect(Triangle.new(1,2,3).number_of_sides).to eq(3)
+    expect(Triangle.new(2,2,3).number_of_sides).to eq(3)
   end
 
   it "needs three sides to be a Triangle" do
@@ -14,6 +14,15 @@ describe "A Triangle " do
     expect{Triangle.new(1)}.to raise_error(ArgumentError)
     expect{Triangle.new(1,2)}.to raise_error(ArgumentError)
     expect{Triangle.new(1,2,3,4)}.to raise_error(ArgumentError)
+  end
+
+  it "should be closed" do
+    expect{Triangle.new(1,2,4)}.to raise_error(ArgumentError)
+    expect{Triangle.new(2,2,4)}.to raise_error(ArgumentError)
+  end
+
+  it "should have all sides as numbers" do
+    expect{Triangle.new(1,"patata",4)}.to raise_error(ArgumentError)
   end
 
 end
--- triangle.rb
+++ triangle.rb
@@ -1,17 +1,38 @@
 require './polygon'
 
 class Triangle<Polygon
+  NUMBER_OF_SIDES=3
+
   def initialize(*sides)
     check_sides(sides)
   end
 
   def number_of_sides
-    3
+    NUMBER_OF_SIDES
   end
 
 private 
+  
   def check_sides sides
-    raise ArgumentError.new("Not enough sides") if sides.size < 3
-    raise ArgumentError.new("Too many sides") if sides.size > 3
+    check_sides_are_numbers sides
+    check_number_of_sides sides
+    check_is_closed sides
+  end
+
+  def check_sides_are_numbers sides
+    error=ArgumentError.new("Non  numeric side length")
+    not_numeric=sides.select{|side| side.class!=Fixnum }.size != 0
+    raise error if not_numeric
+  end
+
+  def check_number_of_sides sides
+    raise ArgumentError.new("Not enough sides") if sides.size < NUMBER_OF_SIDES
+    raise ArgumentError.new("Too many sides") if sides.size > NUMBER_OF_SIDES
+  end
+
+  def check_is_closed sides
+    error=ArgumentError.new("Not closed")
+    not_closed=(sides[0]+sides[1]<= sides[2])|| (sides[0]+sides[2]<= sides[1])|| (sides[2]+sides[1]<= sides[0])
+    raise error if not_closed 
   end
 end
