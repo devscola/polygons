1dd6aec665f3a69f8b2fcdb0edfa5cf7f59d20ea
XaV1Uzz <xaviuzz@gmail.com>
2014-07-09 12:10:14 +0200

    Pulling up to Polygon

--- polygon.rb
+++ polygon.rb
@@ -1,4 +1,38 @@
 class Polygon
 
-  
+  def initialize(*sides)
+    set_number_of_sides
+    check_sides(sides)
+  end
+
+  def number_of_sides
+    @number_of_sides
+  end
+
+  def check_sides sides
+    check_sides_are_positive_numbers sides
+    check_number_of_sides sides
+    check_is_closed sides
+  end
+private
+
+  def set_number_of_sides
+    raise "not implemented"
+  end
+
+  def check_sides_are_positive_numbers sides
+    error=ArgumentError.new("Non positive numeric side length")
+    not_numeric=sides.select{|side| (side.class!=Fixnum)||(side<=0) }.size != 0
+    raise error if not_numeric
+  end
+
+  def check_number_of_sides sides
+    raise ArgumentError.new("Not enough sides") if sides.size < @number_of_sides
+    raise ArgumentError.new("Too many sides") if sides.size > @number_of_sides
+  end
+
+  def check_is_closed sides
+    raise "not implemented" 
+  end
+
 end
--- triangle.rb
+++ triangle.rb
@@ -1,33 +1,11 @@
 require './polygon'
 
 class Triangle<Polygon
-  NUMBER_OF_SIDES=3
-
-  def initialize(*sides)
-    check_sides(sides)
-  end
-
-  def number_of_sides
-    NUMBER_OF_SIDES
-  end
-
-private 
   
-  def check_sides sides
-    check_sides_are_positive_numbers sides
-    check_number_of_sides sides
-    check_is_closed sides
-  end
 
-  def check_sides_are_positive_numbers sides
-    error=ArgumentError.new("Non positive numeric side length")
-    not_numeric=sides.select{|side| (side.class!=Fixnum)||(side<=0) }.size != 0
-    raise error if not_numeric
-  end
-
-  def check_number_of_sides sides
-    raise ArgumentError.new("Not enough sides") if sides.size < NUMBER_OF_SIDES
-    raise ArgumentError.new("Too many sides") if sides.size > NUMBER_OF_SIDES
+private
+  def set_number_of_sides
+    @number_of_sides = 3   
   end
 
   def check_is_closed sides
