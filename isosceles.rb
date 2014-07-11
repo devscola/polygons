19f641b7ddb31d96418995c0add7a6afec573e15
XaV1Uzz <xaviuzz@gmail.com>
2014-07-11 19:34:38 +0200

    Isosceles its defined with a pair of sides

--- isosceles.rb
+++ isosceles.rb
@@ -1,4 +1,7 @@
 require './triangle'
 
 class Isosceles<Triangle
+  def initialize (repeated_side,unique_side)
+    
+  end
 end
--- spec/isosceles_spec.rb
+++ spec/isosceles_spec.rb
@@ -2,6 +2,16 @@ require './isosceles'
 
 describe "Isosceles" do
   it "its a Triangle" do
-    expect(Isosceles.new(2,3,3)).to be_a(Triangle)   
+    expect(Isosceles.new(2,3)).to be_a(Triangle)   
+  end
+
+  it "its defined with a pair of sides" do
+    repeated = 2
+    unique = 3
+    expect do
+      Isosceles.new(repeated,unique)
+    end.to_not raise_error(ArgumentError)   
   end  
+
+
 end
